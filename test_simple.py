e0be360096ddb8b223ed0d9d346cf99694781e5d
Brian Fealy <brfealy@users.noreply.github.com>
2017-04-28 15:52:48 -0400

    spacing

--- test_simple.py
+++ test_simple.py
@@ -4,7 +4,9 @@ import sys
 if __name__ == "__main__":
     thing = sys.argv[1]
     if thing == "B":
+        
         print("Test succeeded, return without error.")
+
     else:
         raise ValueError("Error in test_simple.py.")
 
