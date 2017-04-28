9367f4b10d3573fc806bcbff6b40c0d3ac74f0ee
Brian Fealy <v-brfeal@microsoft.com>
2017-04-28 14:58:28 -0400

    test commit
    to trigger a build

--- test_simple.py
+++ test_simple.py
@@ -4,10 +4,10 @@ import os
 def main():
     x = sys.argv[1]
     if x == "A":
-        print("Test succeeded, return without error")
+        print("Test succeeded, return without error.")
         return 0
     else:
-        raise ValueError("Error in test_simple.py")
+        raise ValueError("Error in test_simple.py.")
 
 if __name__ == "__main__":
     main()
