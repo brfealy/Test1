a4761f3c5eaaad0438a175007f5f514a6f1df4b7
Brian Fealy <brfealy@users.noreply.github.com>
2017-04-28 15:33:30 -0400

    fix

--- test_simple.py
+++ test_simple.py
@@ -5,7 +5,6 @@ if __name__ == "__main__":
     thing = sys.argv[1]
     if thing == "B":
         print("Test succeeded, return without error.")
-        return 0
     else:
         raise ValueError("Error in test_simple.py.")
 
